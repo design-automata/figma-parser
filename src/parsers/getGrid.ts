6803ef9137d5585cfd3f042e8d3baf7e65d7af9f
Edison Chee <edisonchee@live.com>
2021-05-14 15:24:20 +0800

    getGrid CSS

--- src/parsers/getGrid.ts
+++ src/parsers/getGrid.ts
@@ -10,8 +10,17 @@ export const getGrid = async (fileStyles) => {
   .then((data: any) => {
     const docArr: any = Object.values(data.nodes).map((node: any) => node.document);
     let grid = {};
-    docArr.forEach(doc => {
-      grid[doc.name] = doc.layoutGrids
+    docArr.forEach(({ name, layoutGrids }) => {
+      let columnLayout = layoutGrids.find(grid => grid.pattern === "COLUMNS");
+      let rowLayout = layoutGrids.find(grid => grid.pattern === "ROWS");
+
+      grid[name] = {
+        gridTemplateColumns: `repeat(${columnLayout.count}, ${columnLayout.sectionSize}px)`,
+        columnGap: `${columnLayout.gutterSize}px`,
+        gridAutoRows: rowLayout ? `${rowLayout.sectionSize}px` : '',
+        rowGap: rowLayout ? `${rowLayout.gutterSize}px` : '',
+        columnOffset: `${columnLayout.offset}px`
+      }
     })
     return grid;
   })
